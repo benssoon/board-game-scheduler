af63c39aaa0e74c5ed550939480c819423f94a88
ben <benjamin.zelinsky@gmail.com>
2025-10-09 16:09:30 +0200

    Set up new Spring project in backend directory.

---
+++ backend/firey-leaf-events/src/main/java/nl/benzelinsky/fireyleafevents/FireyLeafEventsApplication.java
@@ -0,0 +1,13 @@
+package nl.benzelinsky.fireyleafevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireyLeafEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FireyLeafEventsApplication.class, args);
+    }
+
+}
