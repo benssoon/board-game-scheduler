e35bf02bddd2f10d29d4788cd8a538ae93f7cfc6
ben <benjamin.zelinsky@gmail.com>
2025-10-11 17:26:52 +0200

    Make a simple card that for now just says Halloween.

---
+++ frontend/src/components/eventCard/EventCard.jsx
@@ -0,0 +1,13 @@
+import './EventCard.css';
+
+function EventCard() {
+    return (
+        <>
+            <div className="halloween">
+                <h1>Halloween</h1>
+            </div>
+        </>
+    )
+}
+
+export default EventCard;
