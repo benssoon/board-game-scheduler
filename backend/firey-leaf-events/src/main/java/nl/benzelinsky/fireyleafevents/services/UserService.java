dbc337f1efc7f8c7f837b2008e1128e52c3cf71e
ben <benjamin.zelinsky@gmail.com>
2025-10-13 12:00:10 +0200

    Implement POST and GET requests for user in controller and service layer.

--- backend/firey-leaf-events/src/main/java/nl/benzelinsky/fireyleafevents/services/UserService.java
+++ backend/firey-leaf-events/src/main/java/nl/benzelinsky/fireyleafevents/services/UserService.java
@@ -1,5 +1,9 @@
 package nl.benzelinsky.fireyleafevents.services;
 
+import nl.benzelinsky.fireyleafevents.dtos.UserInputDto;
+import nl.benzelinsky.fireyleafevents.dtos.UserOutputDto;
+import nl.benzelinsky.fireyleafevents.mappers.UserMapper;
+import nl.benzelinsky.fireyleafevents.models.User;
 import nl.benzelinsky.fireyleafevents.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +16,18 @@ public class UserService {
         this.repository = repository;
     }
 
+    // Create user
+    public UserOutputDto createUser(UserInputDto userInputDto) {
+        User user = UserMapper.toEntity(userInputDto);
+        this.repository.save(user);
+        return UserMapper.toOutputDto(user);
+    }
+
+    // Get user by id
+    public UserOutputDto getUserById(Long id) {
+        User user = this.repository.getUserById(id);
+        return UserMapper.toOutputDto(user);
+        // EXCEPTION HANDLING!
+    }
 
 }
