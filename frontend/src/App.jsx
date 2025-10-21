0574c6ac360b469d4fff26760bdcd00f7a5baf30
ben <benjamin.zelinsky@gmail.com>
2025-10-21 22:21:54 +0200

    Make create event form controlled and send the actual input data.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,22 @@ import EventCard from './components/eventCard/EventCard.jsx';
 import axios from 'axios';
 
 function App() {
+    const devApiUrl = 'http://localhost:8080';
+
     //<editor-fold desc="State">
     const [formState, setFormState] = useState({
         userId: 0,
         eventId: 0,
     });
-    const [userId, setUserId] = useState(0);
-    const [eventId, setEventId] = useState(0);
+
+    const [eventFormState, setEventFormState] = useState({
+        title: '',
+        location: '',
+        isFull: false,
+    });
+
+    const [user, setUser] = useState({});
     const [event, setEvent] = useState({});
-    const devApiUrl = 'http://localhost:8080';
     //</editor-fold>
 
     //<editor-fold desc="Handlers">
@@ -29,6 +36,18 @@ function App() {
             [changedFieldName]: newValue,
         });
     }
+
+    function handleCreateChange(e) {
+        const changedFieldName = e.target.name;
+        const newValue = e.target.value;
+        console.log(changedFieldName);
+        console.log(newValue);
+
+        setEventFormState({
+            ...eventFormState,
+            [changedFieldName]: newValue,
+        });
+    }
     //</editor-fold>
 
     //<editor-fold desc="HTTP requests">
@@ -36,8 +55,15 @@ function App() {
         try {
             const response = await axios.get(`${devApiUrl}/${type}s/${id}`);
             console.log(response);
-            if (type === 'event') {
-                setEvent(response.data);
+            switch (type) {
+                case 'event':
+                    setEvent(response.data);
+                    break;
+                case 'user':
+                    setUser(response.data);
+                    break;
+                default:
+                    break;
             }
         } catch (e) {
             console.error(e.message + ': ' + e.response.data);
@@ -47,13 +73,21 @@ function App() {
     async function createEvent(e) {
         e.preventDefault();
         try {
-            const response = await axios.post(devApiUrl+'/events', {
-                title: 'Arcs',
-                isFull: false,
-            })
+            const response = await axios.post(devApiUrl+'/events', eventFormState)
             console.log(response);
         } catch (e) {
+            const response = e.response.data;
+            const missingKeys = Object.keys(response);
             console.error(e);
+            let errors = [];
+            for (const key in missingKeys) {
+                const missingKey = missingKeys[key]
+                const keyProblem = response[missingKey]
+                const err = `"${missingKey}" ${keyProblem}`;
+                console.error(err);
+                errors.push(err);
+                return errors;
+            }
         }
     }
 
@@ -66,7 +100,18 @@ function App() {
             })
             console.log(response);
         } catch (e) {
+            const response = e.response.data;
+            const missingKeys = Object.keys(response);
             console.error(e);
+            let errors = [];
+            for (const key in missingKeys) {
+                const missingKey = missingKeys[key]
+                const keyProblem = response[missingKey]
+                const err = `"${missingKey}" ${keyProblem}`;
+                console.error(err);
+                errors.push(err);
+                return errors;
+            }
         }
     }
 //</editor-fold>
@@ -85,14 +130,28 @@ function App() {
                 possibleTimes={event.possibleTimes}
             />
 
+            {/* Create Event */}
             <form>
                 <label htmlFor="eventTitle">Event title:</label>
-                <input type="text" id="eventTitle"/>
-                <label htmlFor="numberPlayers">Number of players:</label>
-                <input type="number" id="numberPlayers" />
+                <input
+                    type="text"
+                    name="title"
+                    id="eventTitle"
+                    value={eventFormState.title}
+                    onChange={handleCreateChange}
+                />
+                <label htmlFor="eventLocation">Location:</label>
+                <input
+                    type="text"
+                    name="location"
+                    id="eventLocation"
+                    value={eventFormState.location}
+                    onChange={handleCreateChange}
+                />
                 <button type="button" onClick={createEvent}>Submit</button>
             </form>
 
+            {/* Create User */}
             <form>
                 <label htmlFor="name">Name:</label>
                 <input type="text" id="name"/>
@@ -101,6 +160,7 @@ function App() {
                 <button type="button" onClick={createUser}>Submit</button>
             </form>
 
+            {/* Get Event */}
             <form>
                 <label htmlFor="eventId">Event ID:</label>
                 <input
@@ -112,6 +172,8 @@ function App() {
                 />
                 <button type="button" onClick={() => handleClick('event', 'eventId')}>Get event</button>
             </form>
+
+            {/* Get User */}
             <form>
                 <label htmlFor="UserId">User ID:</label>
                 <input
