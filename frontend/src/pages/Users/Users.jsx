448911167eab3309947ecaa87ac4abc5102b77ce
ben <benjamin.zelinsky@gmail.com>
2025-12-22 21:18:56 +0100

    Display all users on the Users page.

--- frontend/src/pages/Users/Users.jsx
+++ frontend/src/pages/Users/Users.jsx
@@ -1,10 +1,20 @@
 import './Users.css';
+import {useState} from 'react';
+import DisplayGrid from '../../components/DisplayGrid/DisplayGrid.jsx';
 
 function Users() {
+    const [updated, setUpdated] = useState(0);
+    const [param, setParam] = useState('');
     return (
         <>
             <h2>Users</h2>
-
+            <section className="categoryBox">
+                <DisplayGrid
+                    type="user"
+                    updated={updated}
+                    param={param}
+                />
+            </section>
         </>
     );
 }
