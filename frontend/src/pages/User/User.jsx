95daa1684ed70db5d04f5bf92ed77a7ab8e489f7
ben <benjamin.zelinsky@gmail.com>
2026-01-05 14:37:33 +0100

    Add button to allow an admin to delete a user.

--- frontend/src/pages/User/User.jsx
+++ frontend/src/pages/User/User.jsx
@@ -1,7 +1,7 @@
 import './User.css';
 import {useContext, useEffect, useState} from 'react';
 import {AuthContext} from '../../context/AuthContext.jsx';
-import {Link, Navigate, useParams} from 'react-router-dom';
+import {Link, Navigate, redirect, useNavigate, useParams} from 'react-router-dom';
 import useFetch from '../../helpers/useFetch.js';
 import InfoBox from '../../components/InfoBox/InfoBox.jsx';
 import axios from 'axios';
@@ -10,8 +10,12 @@ import Roles from '../../components/Roles/Roles.jsx';
 import Detail from '../../components/Detail/Detail.jsx';
 
 function User() {
+    const [errorNotification, setErrorNotification] = useState(null);
+    const [successNotification, setSuccessNotification] = useState(null);
+
     const {username} = useParams();
-    const {user: currentUser} = useContext(AuthContext);
+    const navigte = useNavigate();
+    const {user: currentUser, isAdmin} = useContext(AuthContext);
     const shouldRedirect = currentUser?.username === username
     const token = localStorage.getItem('token');
     const {data: user, loading, error} = useFetch(shouldRedirect ? null : `/users/${username}`, {
@@ -28,10 +32,33 @@ function User() {
         return <Navigate to={"/profile"} replace/>
     }
 
+    async function handleDeleteUser(e) {
+        e.preventDefault();
+        try {
+            const result = await axios.delete(`${API}/users/${username}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            console.log(result);
+            setSuccessNotification(`User ${username} has been deleted!`);
+            navigte('/users');
+        } catch (er) {
+            const response = er.response;
+            console.log(response);
+            setErrorNotification(response.data);
+        }
+    }
+
     return (
         user ?
             <>
                 <h2>This is {username}'s page.</h2>
+                {isAdmin && <InfoBox
+                    type={'delete'}
+                >
+                    <button type={'button'} onClick={handleDeleteUser}>Delete User</button>
+                </InfoBox>}
                 <InfoBox
                     type={'roles'}
                 >
