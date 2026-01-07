6dabd9cc4f002ebff057d7ad49a79f80bb970924
ben <benjamin.zelinsky@gmail.com>
2026-01-07 12:20:19 +0100

    Fix game filter so that More similar titles are given.

--- frontend/src/components/FiltersBox/FiltersBox.jsx
+++ frontend/src/components/FiltersBox/FiltersBox.jsx
@@ -5,25 +5,28 @@ import Pill from '../Pill/Pill.jsx';
 
 import {useEffect, useState} from 'react';
 
-function FiltersBox({setParam}) {
+function FiltersBox({setParam, searchingFor}) {
     const [filters, setFilters] = useState([]);
 
+    // Set the url parameter for filtering events by game.
     useEffect(() => {
-        let p = '';
+        let urlParameter = '';
         for (const filtersKey in filters) {
-            if (p && filters[filtersKey].param) {
-                p=p+'&'
+            if (urlParameter && filters[filtersKey].param) {
+                urlParameter = urlParameter + '&' // If there is more than one filter given, separate them by '&' in the parameter.
             }
-            p = p + filters[filtersKey].param;
+            urlParameter = urlParameter + filters[filtersKey].param;
         }
-        if (p) {
-            setParam(p);
+        if (urlParameter) {
+            setParam(urlParameter);
         }
     }, [filters]);
+
     return (
         <div className="filtersBox">
             <SearchBar
                 setFilters={setFilters}
+                searchingFor={searchingFor}
             />
             <ul className="pills">
                 {filters.map((item) => {
