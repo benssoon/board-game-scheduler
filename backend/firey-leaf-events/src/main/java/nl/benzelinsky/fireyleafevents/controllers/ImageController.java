6587942b3a5aae19dbbacdb95d36acf175c4926c
ben <benjamin.zelinsky@gmail.com>
2026-01-27 16:22:14 +0100

    Add get request to get image.

--- backend/firey-leaf-events/src/main/java/nl/benzelinsky/fireyleafevents/controllers/ImageController.java
+++ backend/firey-leaf-events/src/main/java/nl/benzelinsky/fireyleafevents/controllers/ImageController.java
@@ -1,20 +1,27 @@
 package nl.benzelinsky.fireyleafevents.controllers;
 
 import nl.benzelinsky.fireyleafevents.services.ImageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
 
 @RestController
 @RequestMapping("/images")
 public class ImageController {
 
     private final ImageService imageService;
+    @Value("${file.upload-dir}")
+    private String uploadDirectory;
+
 
     public ImageController(ImageService service) {
         this.imageService = service;
@@ -29,5 +36,22 @@ public class ImageController {
             throw new RuntimeException(e);
         }
         return ResponseEntity.ok().body(filename);
+    }
+
+    @GetMapping("/{filename}")
+    public ResponseEntity<Resource> getImage(@PathVariable String filename) {
+        try {
+            Path filePath = Path.of(uploadDirectory).resolve(filename);
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                return ResponseEntity.ok()
+                        .contentType(MediaType.IMAGE_PNG)
+                        .body(resource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (MalformedURLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
