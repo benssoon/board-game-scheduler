8c2bb9b898f9812d06c39a2eaacd77afb6c44e13
ben <benjamin.zelinsky@gmail.com>
2026-01-28 10:53:14 +0100

    Start intallation guidelines in README.org. Change directory names to reflect the new title FiLoGames.

--- backend/firey-leaf-events/src/main/java/nl/benzelinsky/fireyleafevents/exceptions/MayNotChangeUsernameException.java
+++
@@ -1,7 +0,0 @@
-package nl.benzelinsky.fireyleafevents.exceptions;
-
-public class MayNotChangeUsernameException extends RuntimeException {
-    public MayNotChangeUsernameException() {
-        super("You are not allowed to change your username.");
-    }
-}
